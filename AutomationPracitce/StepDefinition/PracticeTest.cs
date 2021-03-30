330283ce837d73970bbd10debca57f9f5990c417
silvershadow993 <silvershadow993@gmail.com>
2021-03-30 13:26:28 +0530

    Added new project files
    Added new files

---
+++ AutomationPracitce/StepDefinition/PracticeTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System;
+
+namespace AutomationPracitce.StepDefinition
+{
+    class PracticeTest
+    {
+        IWebDriver driver;
+
+        private readonly string url = "http://automationpractice.com/index.php";
+        private readonly string loginLink = "//a[@title='Log in to your customer account']";
+        private readonly string pageHeadingElement = "//h1[text()='Authentication']";
+
+
+        [SetUp]
+        public void setUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+        }
+
+
+        [Test]
+        public void RegistrationTest()
+        {
+            driver.FindElement(By.XPath(loginLink)).Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            IWebElement pageHeading = driver.FindElement(By.XPath(pageHeadingElement));
+            Assert.AreEqual("AUTHENTICATION", pageHeading.Text);
+            
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
---
+++ AutomationPracitce/StepDefinition/TestDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationPracitce.StepDefinition
+{
+    class TestDemo
+    {
+        IWebDriver driver;
+
+
+        [SetUp]
+        public void setUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void test1()
+        {
+            Assert.Fail();
+        }
+
+       
+        [TearDown]
+        public void tearDown()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+    }
+}
